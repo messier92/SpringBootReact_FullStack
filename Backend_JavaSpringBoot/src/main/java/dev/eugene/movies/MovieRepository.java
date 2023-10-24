38fa61390b174a970df1489e5d4a6cc51b7de579
Han Long Eugene, Goh <hanlong2102@hotmail.com>
2023-10-24 18:20:06 +0800

    Server

---
+++ Backend_JavaSpringBoot/src/main/java/dev/eugene/movies/MovieRepository.java
@@ -0,0 +1,14 @@
+// Data Access layer
+// Talks to the database and gets the data back
+package dev.eugene.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
+}
---
+++ Backend_JavaSpringBoot/src/main/java/dev/eugene/movies/MoviesApplication.java
@@ -0,0 +1,20 @@
+package dev.eugene.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String apiRoot() {
+		return "Hello World!";
+	}
+}
