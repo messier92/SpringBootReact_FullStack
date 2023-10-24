17f32c5bbd0738d185f7f04afe4710c2313f191d
Han Long Eugene, Goh <hanlong2102@hotmail.com>
2023-10-24 22:17:50 +0800

    updated review

--- Backend_JavaSpringBoot/src/main/java/dev/eugene/movies/Review.java
+++ Backend_JavaSpringBoot/src/main/java/dev/eugene/movies/Review.java
@@ -7,16 +7,20 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection="reviews")
+import java.time.LocalDateTime;
+
+@Document(collection = "reviews")
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@AllArgsConstructor @NoArgsConstructor
 public class Review {
-    @Id
     private ObjectId id;
     private String body;
+    private LocalDateTime created;
+    private LocalDateTime updated;
 
-    public Review(String body) {
+    public Review(String body, LocalDateTime created, LocalDateTime updated) {
         this.body = body;
+        this.created = created;
+        this.updated = updated;
     }
 }
--- Backend_JavaSpringBoot/src/main/java/dev/eugene/movies/ReviewService.java
+++ Backend_JavaSpringBoot/src/main/java/dev/eugene/movies/ReviewService.java
@@ -6,17 +6,24 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
 public class ReviewService {
     @Autowired
-    private ReviewRepository reviewRepository;
+    private ReviewRepository repository;
+
     @Autowired
     private MongoTemplate mongoTemplate;
-    public Review createReview(String reviewBody, String imdbId) {
-        Review review = reviewRepository.insert(new Review(reviewBody));
 
-        reviewRepository.insert(review);
-        mongoTemplate.update(Movie.class).matching(Criteria.where("imdbId").is(imdbId)).apply(new Update().push("reviewIds").value(review)).first();
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = repository.insert(new Review(reviewBody, LocalDateTime.now(), LocalDateTime.now()));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviews").value(review))
+                .first();
 
         return review;
     }
